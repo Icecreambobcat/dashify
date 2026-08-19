f2289438060c7321ed91b81e43b912628b3eba90
Icecreambobcat <icekingrex123@gmail.com>
2026-08-19 21:15:48 +1000

    Parser unit tests
    Adds some initial unit testing for correct config file fetching.

---
+++ tests/parser_test.py
@@ -0,0 +1,7 @@
+from modules.parser import get_config_path
+
+def test_get_config_path():
+    assert get_config_path().is_file()
+
+def test_config_file():
+    assert get_config_path().name == "dashify.toml"
